d4161d54b58d45ecec7fc86e8c60378a3e9919ba
happiness03 <stevenhappiness7@gmail.com>
2015-05-21 18:22:54 +0100

    MY C++ ASSIGNMENT

---
+++ assignment4.cpp
@@ -0,0 +1,58 @@
+/*
+ *  Question4.cpp
+ *
+ *  Name:           [ HAPPINESS STEPHEN ETIM]
+ *  Department:     [STATISTICS ]
+ *  Matric. No:     [ PDS/2013/2876 ]
+ *
+ *  Program that inputs a five-digit integer,
+ *  separates the integer into its digits and prints them separated by three spaces each
+ *
+ */
+ 
+ #include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "*********\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*********\n";
+	     cout << "   ** \n"
+	         << " *    * \n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << " *    *\n"
+	         << "   **  \n";
+	     cout << "  *   \n"
+	          << " ***  \n"
+	          << "***** \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n";
+	     cout << "    *   \n"
+	          << "   * *    \n"
+	         << " *     *  \n"
+	        << "*       *\n"
+	        << " *     * \n"
+	        << "   * *   \n"
+	        << "    *    \n";
+	        
+	        return 0;
+	        system("PAUSE");
+	        
+	        return 'EXIT_SUCCESS';
+}
